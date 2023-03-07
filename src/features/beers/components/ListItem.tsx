d774d4fc2b0ee8f2862658a2a3bd5fe18b96f093
Kulcsar Mate <kulcsarmate@gmail.com>
2023-03-07 21:46:10 +0100

    feat: :sparkles: redirect to beer details

--- src/features/beers/components/ListItem.tsx
+++ src/features/beers/components/ListItem.tsx
@@ -1,12 +1,21 @@
 import { Card, Image } from "semantic-ui-react";
 import styled from "styled-components";
 import { ListItemProps } from "./model";
+import { useNavigate } from "react-router-dom";
 
-export const ListItem = ({ name, image_url, abv }: ListItemProps) => {
+export const ListItem = ({ id, name, image_url, abv }: ListItemProps) => {
+  const navigate = useNavigate();
+
+  const handleLink = (id: number) => {
+    const link = `/beer/${id}`;
+
+    return navigate(link);
+  }
+
   return (
-    <Card centered image>
+    <Card link centered onClick={() => handleLink(id)}>
       <ImageWrapper>
-        <Image src={image_url} size="tiny" fluid centered style={{ height: 'fit-content' }} />
+        <Image src={image_url} size="tiny" centered />
       </ImageWrapper>
       <Card.Content>
         <Card.Header textAlign="center">{name}</Card.Header>
